0ea6de482950e71717c5b1d7db3e6af7d11202bf
zhenghuachen <z1820418897@163.com>
2019-08-29 11:49:40 +0800

    init

---
+++ flutter_webrtc/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("webrtc应用手机客户端"),),
+      body: Center(
+        child: Container(
+          child: Text("你好世界"),
+        ),
+      ),
+    );
+  }
+}
